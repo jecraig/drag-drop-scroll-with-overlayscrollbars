033729f4cad1b64242cd20224a734b8a53cbf74e
Jacob Craig <jacob.everett.craig@gmail.com>
2020-05-07 00:08:29 -0600

    adding a button to manually reset the parent scroller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,6 +36,10 @@ export class AppComponent implements AfterViewInit {
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
   ngAfterViewInit() {
+    this.setParentScroll();
+  }
+
+  setParentScroll() {
     this.dropList._dropListRef.withScrollableParents([
       this.scrollableParent
         .osTarget()
